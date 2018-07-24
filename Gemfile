2683eb1d9952cb86d045f9c26e83383334775b8f
Kalys Osmonov <kalys@osmonov.com>
2018-07-24 23:02:13 +0600

    Use dry-configurable instead of custom class

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source 'https://rubygems.org'
 
 ruby '2.5.1'
 
+gem 'dry-configurable'
 gem 'telegram-bot-ruby', '~> 0.8.6'
 
 group :development, :test do
--- lib/rubyata_bot.rb
+++ lib/rubyata_bot.rb
@@ -1,21 +1,17 @@
 # frozen_string_literal: true
+require 'dry-configurable'
 
 # RubyataBot is used by telegram.me/rubyata chat for administrative tasks
 module RubyataBot
+  extend Dry::Configurable
+
   ROOT        = Pathname.new(::File.expand_path('../../', __FILE__)).freeze
   ENVIRONMENT = ENV.fetch('BOT_ENV', 'development').freeze
 
-  class << self
-    def config
-      @config ||= Configuration.new
-    end
+  setting :token, ENV['BOT_TOKEN']
+  setting :logger, Logger.new(ENV.fetch('BOT_LOGFILE') { STDOUT })
 
-    def configure
-      yield config
-    end
-
-    def run
-      Runner.new.run
-    end
+  def self.run
+    Runner.new.run
   end
 end
